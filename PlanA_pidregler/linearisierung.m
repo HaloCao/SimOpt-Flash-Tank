b03fc9c74b78e819c280c0d2bc2fd731d910eee7
HaloCao <38354924+HaloCao@users.noreply.github.com>
2019-01-26 11:28:13 +0100

    new version

--- PlanA_pidregler/linearisierung.m
+++ PlanA_pidregler/linearisierung.m
@@ -1,11 +1,11 @@
 clear all;
 load_system('modell')
-%load_system('Regelkreis')
+load_system('Regelkreis')
 x = Simulink.BlockDiagram.getInitialState('modell');
-
-x.signals(1).values=8;
-x.signals(2).values=7;
-x.signals(3).values=4;
-
-u=[0;0;0];
+%Initiale Statewerte
+x.signals(1).values=0.5;%L1,0
+x.signals(2).values=709100;  %P1,0
+x.signals(3).values=40.2;  %L2,0
+%Initiale Eingangswerte
+u=[0;0;0.4];    %Kv2,Kv1,Pel.
 [A,B,C,D]=linmod('modell',x,u);
